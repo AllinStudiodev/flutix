4cf5137b2421f20d2c6b50930da1da0e4a6ad1c5
echonomicon <echo.holmez@gmail.com>
2020-06-01 13:49:51 +0700

    navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
-import 'package:flutix/services/services.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+
+import 'blocs/blocs.dart';
+import 'services/services.dart';
+import 'ui/pages/pages.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,45 +13,16 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RaisedButton(
-                  child: Text("sign Up"),
-                  onPressed: () async {
-                    SignInSignUpResult result = await AuthServices.signUp(
-                        "echo@gmail.com",
-                        "123456",
-                        "echo",
-                        ["Action", "Horror", "Music", "Drama"],
-                        "English");
-
-                    if (result.user == null) {
-                      print(result.message);
-                    } else {
-                      print(result.user.toString());
-                    }
-                  }),
-              RaisedButton(
-                  child: Text("sign In"),
-                  onPressed: () async {
-                    SignInSignUpResult result = await AuthServices.signIn(
-                      "echo@gmail.com",
-                      "1234567",
-                    );
-
-                    if (result.user == null) {
-                      print(result.message);
-                    } else {
-                      print(result.user.toString());
-                    }
-                  })
-            ],
-          ),
+    return StreamProvider.value(
+      value: AuthServices.userStream,
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider(create: (_) => PageBloc()),
+          BlocProvider(create: (_) => UserBloc())
+        ],
+        child: MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: Wrapper(),
         ),
       ),
     );
--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,1 +1,3 @@
+import 'package:flutix/blocs/blocs.dart';
+
 part 'shared_value.dart';
